a0da71e932c8bb3ff5aa36134a94f057916b5af2
Jarugal <jargal620@gmail.com>
2022-05-22 01:44:15 +0000

    firstChanges

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Crud application");
+})
+app.listen(8000,()=>{console.log("Server is running on http://localhost:${8000}")});
